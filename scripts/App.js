7f0e4d7b82c4a25e5100144696fecfb26ee499b3
Timothee AUPETIT <timothee.aupetit@gmail.com>
2017-06-13 16:09:13 +0200

    changed img source

--- scripts/App.js
+++ scripts/App.js
@@ -1,16 +1,16 @@
 var images = [
-    "./images/black_square.jpg",
-    "./images/camera.png",
-    "./images/cross.png",
-    "./images/dollar.png",
-    "./images/eyes.png",
-    "./images/herringbone.png",
-    "./images/red_dice.jpg",
-    "./images/rotated.png",
-    "./images/sourire.png",
-    "./images/tissage.png",
-    "./images/white_square.png",
-    "./images/windows.jpg"
+    "https://pbs.twimg.com/profile_images/774297805116407808/NhxKlL4B.jpg",
+    "http://4vector.com/i/free-vector-square-and-octagons-clip-art_103997_Square_And_Octagons_clip_art_medium.png",
+    "https://s-media-cache-ak0.pinimg.com/236x/97/4b/a5/974ba58f9dbf6102a347ab76d831e049.jpg",
+    "http://3.bp.blogspot.com/-33q6sur1VoI/VgcLicdIt4I/AAAAAAAAE1c/AmbOGbTSe0s/s1600/unnamed.png",
+    "http://images.clipartpanda.com/square-clip-art-ted_Eyes_Crossed.png",
+    "http://is3.mzstatic.com/image/thumb/Purple127/v4/8d/67/8b/8d678be2-3b6e-e2e5-474e-e0ce606d8f2b/source/1200x630bb.jpg",
+    "http://www.janehaslemgallery.com/pages/exhibitions/2014_a_look_at_the_past_grey/images/homage-to-the-square-by-josef-albers-at-jane-haslem-gallery.jpg",
+    "http://www.dibujosparapintar.com/archivos/cuadriculas/nivel_muy_facil.png",
+    "http://is5.mzstatic.com/image/thumb/Purple71/v4/90/ae/1b/90ae1b97-762d-82fa-1ff9-bcc76435ea88/source/1200x630bb.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/7/71/Weaved_truncated_square_tiling.png",
+    "https://s.w.org/images/core/emoji/2/svg/25b6.svg",
+    "http://www.politicalmetaphors.com/wp-content/uploads/2015/04/blog-shapes-square-windows.jpg"
 ];
 
 new Memory('game', images);
